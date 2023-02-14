c5e29032b6c0ec0225f614aaf70a75f074a397fb
wiidD <ddong86800@163.com>
2023-02-14 22:58:33 +0800

    complete charge instead request

--- pages/charge/charge.js
+++ pages/charge/charge.js
@@ -1,4 +1,7 @@
 // pages/charge/charge.js
+var app = getApp();
+import Dialog from '@vant/weapp/dialog/dialog';
+
 Page({
 
     /**
@@ -6,7 +9,125 @@ Page({
      */
     data: {
 
+        dd : 2,
+        dddialog : false,
+        times: ['请选择','1小时', '2小时', '3小时', '4小时', '5小时'],
+        chargetime : 0,
+        alls : [
+            {
+                "name" : "1",
+                "isfree" : true,
+                "isbusy" : false,
+                "time" : 3,
+            },
+            {
+                "name" : "2",
+                "isfree" : false,
+                "isbusy" : true,
+                "time" : 3000000
+            },
+            {
+                "name" : "3",
+                "isfree" : true,
+                "isbusy" : false,
+                "time" : 5
+            },
+            {
+                "name" : "4",
+                "isfree" : false,
+                "isbusy" : true,
+                "time" : 0
+            },
+            {
+                "name" : "5",
+                "isfree" : false,
+                "isbusy" : true,
+                "time" : 43252352345
+            },
+            {
+                "name" : "6",
+                "isfree" : true,
+                "isbusy" : false,
+                "time" : 231321312312
+            },
+            {
+                "name" : "7",
+                "isfree" : true,
+                "isbusy" : false,
+                "time" : 3456534
+            },
+            {
+                "name" : "8",
+                "isfree" : true,
+                "isbusy" : false,
+                "time" : 3465
+            },
+            {
+                "name" : "9",
+                "isfree" : true,
+                "isbusy" : false,
+                "time" : 342432
+            },
+            {
+                "name" : "10",
+                "isfree" : true,
+                "isbusy" : false,
+                "time" : 2344324
+            },
+            {
+                "name" : "11",
+                "isfree" : true,
+                "isbusy" : false,
+                "time" : 432423
+            },
+            {
+                "name" : "12",
+                "isfree" : true,
+                "isbusy" : false,
+                "time" : 3412412341
+            },
+        ]
     },
+
+    onChange(event) {
+        //const { picker, value, index } = event.detail;
+        //Toast(`当前值：${value}, 当前索引：${index}`);
+        console.log(event);
+        //console.log(event.detail.value);
+        if(event.detail.index === 0) {
+            this.setData({
+                chargetime: 0
+            })
+        } else {
+            this.setData({
+                chargetime: event.detail.value[0] * 60 * 60 * 1000
+            })
+        }
+      },
+
+      //点击充电按钮，
+    ddpay : function() {
+        this.setData({
+            dddialog : true
+        })
+    },
+
+    //点击确认和取消按钮，
+    ddcancle : function(res) {
+        this.setData({
+            dddialog : false
+        })
+        //console.log("au");
+        //console.log(res);
+        if(res.detail === "confirm") {
+            console.log(this.data.chargetime);
+            //发送给后端，等后面再写
+        }
+    },
+    /*好像没什么用，没看懂
+    ddconfirm : function() {
+        console.log("fas");
+    },*/
 
     /**
      * 生命周期函数--监听页面加载
--- pages/friends/friends.js
+++ pages/friends/friends.js
@@ -1,12 +1,44 @@
 // pages/friends/friends.js
+
+import Toast from '@vant/weapp/toast/toast';
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        istrue : true,
+        columns: ['杭州', '宁波', '温州', '嘉兴', '湖州'],
     },
+
+    dd_send : function() {
+        console.log(this.data.information);
+        wx.request({
+          url: 'https://api.chatgpt.com/v1/query',
+          header : {
+            Authorization : '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'
+          },
+          data: {
+              model : 'text-davinci-003',
+              prompt : this.data.information
+          },
+          method : 'post',
+          success : function(res) {
+              console.log("abc");
+              console.log(res);
+          }
+        })
+    },
+
+    InformationChange(event) {
+        // event.detail 为当前输入的值
+        //console.log(event);
+        this.setData({
+            information:event.detail
+        })
+        console.log(this.data.information);
+      },
 
     /**
      * 生命周期函数--监听页面加载
